8dcb281c26982e9c650f15d47f90e64f49a295c4
Ksenox <akseli.muhonen@proton.me>
2026-03-17 14:27:53 +0200

    chore: removed "LOCAL_" prefix from .env vars

--- scripts/gen-db-if.sh
+++ scripts/gen-db-if.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 
 source .env
 
-npx kysely-codegen --dialect mysql --url mysql://${LOCAL_DB_USERNAME}:${LOCAL_DB_PASSWORD}@127.0.0.1:3306/newsagg --out-file ./src/server/db/types.ts
+npx kysely-codegen --dialect mysql --url "mysql://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}" --out-file ./src/server/db/types.ts
--- scripts/start-db.sh
+++ scripts/start-db.sh
@@ -18,23 +18,21 @@ if ! test -r .env; then
 fi
 
 source .env
-
-DB_NAME="newsagg"
 DOCKER_DB_NAME="${DB_NAME}-db"
 
 if docker ps -a --format '{{.Names}}' | grep -q "^${DOCKER_DB_NAME}$"; then
-  docker start newsagg-db
+  docker start $DOCKER_DB_NAME
 else
   docker run -d \
     --name "${DOCKER_DB_NAME}" \
-    -e MYSQL_ROOT_PASSWORD="${LOCAL_DB_PASSWORD}" \
+    -e MYSQL_ROOT_PASSWORD="${DB_PASSWORD}" \
     -e MYSQL_DATABASE="${DB_NAME}" \
-    -e MYSQL_USER="${LOCAL_DB_USERNAME}" \
-    -e MYSQL_PASSWORD="${LOCAL_DB_PASSWORD}" \
+    -e MYSQL_USER="${DB_USERNAME}" \
+    -e MYSQL_PASSWORD="${DB_PASSWORD}" \
     -p 3306:3306 \
     -v news-aggr-db-data:/var/lib/mysql \
     -v ./src/server/db/schema.sql:/docker-entrypoint-initdb.d/init.sql \
     mysql:8 \
-  && docker start newsagg-db
+  && docker start $DOCKER_DB_NAME
 fi
 
