394b7ad751965a7222b6912c587010a0ac6cef6d
Ksenox <akseli.muhonen@proton.me>
2026-03-19 11:58:21 +0200

    chore: removed template router
    chore: barrel for db types
    
    chore: barrel for db types

--- scripts/gen-db-if.sh
+++ scripts/gen-db-if.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 
 source .env
 
-npx kysely-codegen --dialect mysql --url "mysql://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}" --out-file ./src/server/db/types.ts
+npx kysely-codegen --dialect mysql --url "mysql://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}" --out-file ./src/server/db/gen_types.ts
