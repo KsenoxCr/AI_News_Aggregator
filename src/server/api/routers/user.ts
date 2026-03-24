6e4ff4b26da24bdcad9412c303adfa51c7f3909f
Ksenox <akseli.muhonen@proton.me>
2026-03-24 13:47:42 +0200

    refactor(userRouter): commented out get getDetails as likely redundant

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -1,22 +1,24 @@
 import { z } from "zod";
-import { createTRPCRouter, protectedProcedure, protectedTranslatedProcedure } from "~/server/api/trpc";
+import { createTRPCRouter, protectedTranslatedProcedure } from "~/server/api/trpc";
 import { db } from "~/server/db/db";
 import { changeLanguageSchema, EditPreferencesSchemaFactory } from "~/lib/validators/user";
 
 export const userRouter = createTRPCRouter({
-    getDetails: protectedProcedure
-        .query(async ({ ctx }) => {
-            const user = await db
-                .selectFrom('users')
-                .select(["email", "preferences", "language", "selected_agent"])
-                .where('id', '=', ctx.session.user.id)
-                .executeTakeFirstOrThrow()
-
-            return {
-                status: "success",
-                ...user
-            }
-        }),
+    // TODO: Uncomment or discard depending on demand (whether TRPCContext + caching suffices)
+    //
+    // getDetails: protectedProcedure
+    //     .query(async ({ ctx }) => {
+    //         const user = await db
+    //             .selectFrom('users')
+    //             .select(["email", "preferences", "locale", "news_language", "selected_agent"])
+    //             .where('id', '=', ctx.session.user.id)
+    //             .executeTakeFirstOrThrow()
+    //
+    //         return {
+    //             status: "success",
+    //             ...user
+    //         }
+    //     }),
     editPreferences: protectedTranslatedProcedure
         .input(z.unknown())
         .mutation(async ({ ctx, input }) => {
