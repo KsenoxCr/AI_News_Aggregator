0877729f42db27e0d908b8ab2b5bf33231bdea55
Ksenox <akseli.muhonen@proton.me>
2026-03-24 12:29:43 +0200

    fix: import path

--- src/lib/validators/user.ts
+++ src/lib/validators/user.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 import { MAX } from "~/config/business";
-import { SUPPORTED_LOCALES } from "../il8n";
 import type { TFn } from "./types";
+import { SUPPORTED_LOCALES } from "../i18n/routing";
 
 export const EditPreferencesSchemaFactory = (t: TFn) =>
     z.string()
