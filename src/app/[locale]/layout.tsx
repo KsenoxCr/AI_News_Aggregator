15472b599bc245cc31aa3ea0515de05c2e11e753
Ksenox <akseli.muhonen@proton.me>
2026-03-26 10:36:33 +0200

    chore: next-themes wrapper into LocaleLayout

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,28 +1,52 @@
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { TRPCReactProvider } from "~/trpc/react";
 import { cn } from "~/lib/utils";
 import { Geist, Figtree } from "next/font/google";
 import "~/styles/globals.css";
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
+import { notFound } from "next/navigation";
+import { routing } from "~/lib/i18n/routing";
+import { ThemeProvider } from "next-themes";
 
-const figtree = Figtree({subsets:['latin'],variable:'--font-sans'});
+const figtree = Figtree({ subsets: ["latin"], variable: "--font-sans" });
 
 const geist = Geist({
-    subsets: ["latin"],
-    variable: "--font-geist-sans",
+  subsets: ["latin"],
+  variable: "--font-geist-sans",
 });
 
-export default function LocaleLayout({
-    children,
-}: Readonly<{ children: React.ReactNode }>) {
-    return (
-        <html className={cn(geist.variable, "font-sans", figtree.variable)} lang="en">
-            <body>
-                <TRPCReactProvider>
-                    {children}
-                    <ReactQueryDevtools initialIsOpen={false} />
-                </TRPCReactProvider>
-            </body>
-        </html>
-    );
+export default async function LocaleLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: Promise<{ locale: string }>;
+}) {
+  const { locale } = await params;
+  if (!routing.locales.includes(locale as any)) notFound();
+
+  const messages = await getMessages();
+
+  return (
+    <html
+      className={cn(geist.variable, "font-sans", figtree.variable, "bg-accent")}
+      lang={locale}
+    >
+      <body>
+        <TRPCReactProvider>
+          <NextIntlClientProvider messages={messages}>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="dark"
+              disableTransitionOnChange
+            >
+              {children}
+            </ThemeProvider>
+          </NextIntlClientProvider>
+          <ReactQueryDevtools initialIsOpen={false} />
+        </TRPCReactProvider>
+      </body>
+    </html>
+  );
 }
