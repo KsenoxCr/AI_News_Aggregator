28578e47bfe68c4f5f6b02500ee5aa13bc7fb9cb
Ksenox <akseli.muhonen@proton.me>
2026-03-29 18:57:12 +0300

    fix(source): insert source and fetch in tandem

--- src/server/api/routers/source.ts
+++ src/server/api/routers/source.ts
@@ -1,96 +1,116 @@
 import { z } from "zod";
 import { MAX } from "~/config/business";
-import { createTRPCRouter, protectedProcedure, protectedTranslatedProcedure } from "~/server/api/trpc";
+import {
+  createTRPCRouter,
+  protectedProcedure,
+  protectedTranslatedProcedure,
+} from "~/server/api/trpc";
 import { db } from "~/server/db/db";
 import { IsDuplicateEntry } from "~/server/lib/util";
-import { AddSourceSchemaFactory, RemoveSourceSchemaFactory } from "~/lib/validators/source";
+import {
+  AddSourceSchemaFactory,
+  RemoveSourceSchemaFactory,
+} from "~/lib/validators/source";
 
 export const sourceRouter = createTRPCRouter({
-    getSources: protectedProcedure
-        .query(async ({ ctx }) => {
-            const sources = await db
-                .selectFrom('sources')
-                .where('user_id', '=', ctx.session.user.id)
-                .execute()
+  getSources: protectedProcedure.query(async ({ ctx }) => {
+    const sources = await db
+      .selectFrom("sources")
+      .where("user_id", "=", ctx.session.user.id)
+      .execute();
 
-            return {
-                status: "success",
-                sources
-            }
-        }),
-    addSource: protectedTranslatedProcedure
-        .input(z.unknown())
-        .mutation(async ({ ctx, input }) => {
-            const schema = AddSourceSchemaFactory(ctx.t)
-            const validated = schema.parse(input)
+    return {
+      status: "success",
+      sources,
+    };
+  }),
+  addSource: protectedTranslatedProcedure
+    .input(z.unknown())
+    .mutation(async ({ ctx, input }) => {
+      const schema = AddSourceSchemaFactory(ctx.t);
+      const validated = schema.parse(input);
 
-            const existingSources = await db
-                .selectFrom('sources')
-                .select(db.fn.count('id').as('count'))
-                .where('user_id', '=', ctx.session.user.id)
-                .executeTakeFirst();
+      const existingSources = await db
+        .selectFrom("sources")
+        .select(db.fn.count("id").as("count"))
+        .where("user_id", "=", ctx.session.user.id)
+        .executeTakeFirst();
 
-            if ((Number(existingSources?.count ?? 0)) >= MAX.sources) {
-                return {
-                    status: "failure",
-                    errorCode: "OUT_OF_BOUNDS",
-                    error: "Maximum source limit reached"
-                };
-            }
+      if (Number(existingSources?.count ?? 0) >= MAX.sources) {
+        return {
+          status: "failure",
+          errorCode: "OUT_OF_BOUNDS",
+          error: "Maximum source limit reached",
+        };
+      }
 
-            const sourceId = crypto.randomUUID();
+      // TODO: Fetch-based feed format detection
 
-            try {
-                await db
-                    .insertInto('sources')
-                    .values({
-                        id: sourceId,
-                        slug: validated.slug,
-                        url: validated.url,
-                        user_id: ctx.session.user.id,
-                    })
-                    .execute();
-            } catch (err: any) {
-                let errorCode, error = null;
+      const sourceId = crypto.randomUUID();
+      const fetchId = crypto.randomUUID();
 
-                if (IsDuplicateEntry(err, "slug")) {
-                    errorCode = "CONFLICT";
-                    error = "Name already in use";
-                }
+      try {
+        await db.transaction().execute(async (trx) => {
+          await trx
+            .insertInto("sources")
+            .values({
+              id: sourceId,
+              slug: validated.slug,
+              url: validated.url,
+              user_id: ctx.session.user.id,
+            })
+            .execute();
 
-                if (IsDuplicateEntry(err, "url")) {
-                    errorCode = "CONFLICT";
-                    error = "URL already in use";
-                }
+          await trx
+            .insertInto("fetches")
+            .values({
+              id: fetchId,
+              source_id: sourceId,
+            })
+            .execute();
+        });
+      } catch (err: any) {
+        let errorCode,
+          error = null;
 
-                return {
-                    status: "failure",
-                    errorCode: errorCode ?? err.code,
-                    error: error ?? err.message
-                };
-            }
+        if (IsDuplicateEntry(err, "slug")) {
+          errorCode = "CONFLICT";
+          error = "Name already in use";
+        }
 
-            return { status: "success", id: sourceId };
-        }),
-    removeSource: protectedTranslatedProcedure
-        .input(z.unknown())
-        .mutation(async ({ ctx, input }) => {
-            const schema = RemoveSourceSchemaFactory(ctx.t)
-            const validated = schema.parse(input)
+        if (IsDuplicateEntry(err, "url")) {
+          errorCode = "CONFLICT";
+          error = "URL already in use";
+        }
 
-            const result = await db
-                .deleteFrom('sources')
-                .where('id', '=', validated)
-                .where('user_id', '=', ctx.session.user.id)
-                .execute();
+        return {
+          status: "failure",
+          errorCode: errorCode ?? err.code,
+          error: error ?? err.message,
+        };
+      }
 
-            if (result[0]!.numDeletedRows === 0n) {
-                return {
-                    status: "failure",
-                    error: "NOT_FOUND"
-                };
-            }
+      return { status: "success", id: sourceId };
+    }),
+  removeSource: protectedTranslatedProcedure
+    .input(z.unknown())
+    .mutation(async ({ ctx, input }) => {
+      const schema = RemoveSourceSchemaFactory(ctx.t);
+      const validated = schema.parse(input);
 
-            return { status: "success" };
-        }),
+      const result = await db
+        .deleteFrom("sources")
+        .where("id", "=", validated)
+        .where("user_id", "=", ctx.session.user.id)
+        .execute();
+
+      if (result[0]!.numDeletedRows === 0n) {
+        return {
+          status: "failure",
+          error: "NOT_FOUND",
+        };
+      }
+
+      return { status: "success" };
+    }),
 });
