f54ac634bc311ef7cf70e4c8d3fa8ed538788d70
Ksenox <akseli.muhonen@proton.me>
2026-04-03 18:22:51 +0300

    feat(newsRouter) (WIP)

--- src/server/api/routers/news.ts
+++ src/server/api/routers/news.ts
@@ -1,27 +1,46 @@
+import { zodToJsonSchema } from "zod-to-json-schema";
 import Parser from "rss-parser";
-import z from "zod";
-import type { DateFormat } from "~/config/business";
-import { dbPut } from "~/lib/db/indexed-db";
+import { v5 as uuidv5 } from "uuid";
+import type { AgentEndpoint, DateFormat } from "~/config/business";
 import { formatDate } from "~/lib/utils";
+import { ClassificationSchema } from "~/lib/validators/news";
 import {
   createTRPCRouter,
   protectedProcedure,
   publicProcedure,
 } from "~/server/api/trpc";
 import { db } from "~/server/db/db";
-import type { Fetches, Sources } from "~/server/db/gen_types";
+import type {
+  Agents,
+  CachedArticles,
+  Fetches,
+  Sources,
+} from "~/server/db/gen_types";
+import type { AgentAdapter, AgentInput } from "~/lib/adapters/agent";
+import { AgentAdapterFactory, AgentInputFactory } from "~/lib/factories/agent";
 
 // TODO: API Error Handling Mechanism for continuing generation where left of
 
 // TODO: Swap "id" as "fetch_id" for semantical clarity or use retrieve distilled sources and fetches as discrete arrays
-type Source = Pick<Sources, "url" | "date_filter_param" | "date_format"> &
-  Pick<Fetches, "id" | "previous_etag"> & {
-    digest_generated: number;
-  };
+type Source = Pick<
+  Sources,
+  "slug" | "url" | "date_filter_param" | "date_format"
+> &
+  Pick<Fetches, "id" | "previous_etag">;
 
-const ArticleSchema = z.object({});
+type Article = Omit<CachedArticles, "used"> & {
+  used: number;
+};
 
-// function DetectCacheMisses(feed: Article[], cached: Article[]) {}
+type ArticleWithCategories = Article & {
+  categories: string[] | null;
+};
+
+type ArticleWithCategory = Article & {
+  category: string | null;
+};
+
+type Agent = Pick<Agents, "url" | "model" | "api_key">;
 
 // TODO: extract unused cached articles
 
@@ -29,18 +48,53 @@ const ArticleSchema = z.object({});
 
 // TODO: update used field for cache items + fetch digest_generated = true
 
-// TODO: Classification & Generation from procedure scope
-
 // TODO: Add misses to cache
 
-async function NormalizeFeed(xmlFeed: string) {
+async function NormalizeFeed(xmlFeed: string, source: Source) {
   const parser = new Parser({
     defaultRSS: 2.0,
   });
 
-  // TODO: Toasted Error handling
+  // TODO: More granular error handling if feasable
 
-  return await parser.parseString(xmlFeed);
+  let rawFeed;
+
+  try {
+    rawFeed = await parser.parseString(xmlFeed);
+  } catch (err) {
+    return {
+      status: "failure",
+      error: {
+        code: "NORMALIZATION_ERROR",
+        message: (err as Error).message,
+        source: source.slug,
+      },
+    };
+  }
+
+  const feed = [] as Article[];
+  const NAMESPACE = uuidv5.URL;
+
+  rawFeed.items.forEach((item) => {
+    const id = uuidv5(`${item.guid}:${source.id}`, NAMESPACE);
+
+    // TODO: Placeholder date incase pubDate missing
+
+    feed.push({
+      id: id,
+      fetch_id: source.id,
+      title: item.title ?? "",
+      link: item.link ?? "",
+      author: item.creator ?? "",
+      published_at: new Date(item.pubDate ?? ""),
+      used: 1,
+    });
+  });
+
+  return {
+    status: "success",
+    feed,
+  };
 }
 
 async function FetchFeed(source: Source, date: Date) {
@@ -64,63 +118,293 @@ async function FetchFeed(source: Source, date: Date) {
   await db
     .updateTable("fetches")
     .set("previous_etag", response.headers.get("ETag"))
-    .set("digest_generated", 0)
     .where("id", "=", source.id)
     .execute();
 
   const xml = await response.text();
 
-  return await NormalizeFeed(xml);
+  return await NormalizeFeed(xml, source);
 }
+
+// Override: array
+
+async function ClassifyArticles(
+  agent: Agent,
+  agentAdapter: AgentAdapter,
+  articles: ArticleWithCategories[],
+) {
+  const serializedArticles = JSON.stringify(articles);
+
+  const outputSchema = ClassificationSchema;
+  const schemaString = JSON.stringify(zodToJsonSchema(outputSchema));
+
+  // TODO: LLM instructions
+
+  const systemPrompt = `
+
+  `;
+
+  const prompt = `
+
+
+    \`\`\`(input)
+    ${serializedArticles}
+    \`\`\`
+
+    \`\`\`(output format)
+    ${schemaString}
+    \`\`\`
+  `;
+
+  // const responseFormat = isOAIAdapter(agentAdapter)
+  //   ? `
+  //
+  // `
+  //   : null;
+
+  const input = AgentInputFactory(
+    agentAdapter.endpoint,
+    agent.model,
+    prompt,
+    systemPrompt,
+  );
+
+  // TODO: response handling logic + retry
+
+  const res = CallAgent(input, agentAdapter, agent.api_key);
+
+  // TODO: Add article_categories to db
+
+  return;
+}
+
+function UnflattedCached(cached: ArticleWithCategory[]) {
+  const unflattened = [] as ArticleWithCategories[];
+  let prevItem: ArticleWithCategories | undefined;
+
+  cached.forEach((article) => {
+    const { category, ...fields } = article;
+
+    // Not previous | categories null
+    if (article.id !== prevItem?.id || !prevItem.categories) {
+      unflattened.push({
+        ...fields,
+        categories: article.category ? [article.category] : null,
+      });
+
+      prevItem = unflattened[unflattened.length - 1];
+    } else {
+      prevItem.categories.push(article.category!);
+    }
+  });
+
+  return unflattened;
+}
+
+function ExtractUnusedArticles(
+  fetched: Article[],
+  cached: ArticleWithCategories[],
+) {
+  // TODO: Add threshold logic: cartesian product (fetched (N) x cached (M)) > threshold ? hashmap O(N): origin array O(NM)
+
+  const fetchedMap = new Map<string, Article>();
+
+  fetched.forEach((item) => fetchedMap.set(item.id, item));
+
+  const cachedCatz = [] as Article[];
+  const cachedUncatz = [] as Article[];
+
+  for (const cArticle of cached) {
+    const fArticle = fetchedMap.get(cArticle.id);
+
+    // Cached but not yet used
+    if (!fArticle || !cArticle.used) {
+      cArticle.categories
+        ? cachedCatz.push(cArticle)
+        : cachedUncatz.push(cArticle);
+    }
+
+    fetchedMap.delete(cArticle.id);
+  }
+
+  const cacheMisses = [] as Article[];
+
+  // Cache misses
+  fetchedMap.forEach((article) => {
+    cacheMisses.push(article);
+  });
+
+  return [cacheMisses, cachedCatz, cachedUncatz];
+}
+
+async function CallAgent(
+  input: AgentInput,
+  adapter: AgentAdapter,
+  apiKey: string,
+) {
+  // Rate limiting logic
+
+  const response: Response = await fetch(input.endpoint, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      ...adapter.authHeaders(apiKey),
+    },
+    ...adapter.buildRequest(input),
+  });
+
+  // TODO: schema unconformance handling
+
+  return adapter.parseResponse(await response.text());
+}
+
+function GenerateDigests(articles: ArticleWithCategories[]) {}
+
+// TODO: SoC
+
+// TODO: Translate error message
+
+// TODO: Wrap in tnx appropriately
 
 export const newsRouter = createTRPCRouter({
   generateFeed: protectedProcedure
     .input(z.date())
-    .query(async ({ ctx, input }) => {
+    .subscription(async function* ({ input, ctx }) {
+      console.log("phase -1");
+
+      const cached_digests = await db
+        .selectFrom("news_digests")
+        .selectAll()
+        .where("user_id", "=", ctx.session.user.id)
+        .where("updated_at", ">=", input)
+        .execute();
+
+      for (const digest of cached_digests) {
+        yield { status: "success", type: "cached", article: digest };
+      }
+
       const sources = (await db
         .selectFrom("sources")
         .where("user_id", "=", ctx.session.user.id)
         .where("enabled", "=", 1)
         .innerJoin("fetches", "fetches.source_id", "sources.id")
         .select([
+          "sources.slug",
           "sources.url",
           "sources.date_filter_param",
           "sources.date_format",
           "fetches.id",
           "fetches.previous_etag",
-          "fetches.digest_generated",
         ])
         .execute()) as Source[];
 
-      // TODO: Translate error message
-
       if (sources.length === 0)
-        return {
+        yield {
           status: "failure",
-          errorCode: "NOT_FOUND",
-          error: "No sources found",
+          error: {
+            code: "NOT_FOUND",
+            message: "No sources found",
+          },
         };
 
+      let fetchedArray = [] as ArticleWithCategories[];
+
       for (const source of sources) {
-        const sourceFeed = await FetchFeed(source, input);
+        const fetchResult = await FetchFeed(source, input);
 
-        const cached = await db
-          .selectFrom("cached_articles")
-          .where("cached_articles.fetch_id", "=", source.id)
-          .innerJoin(
-            "article_categories",
-            "article_categories.article_id",
-            "cached_articles.id",
-          )
-          .selectAll()
-          .execute();
-
-        // TODO: Why segregate processing by sources? vs processing articles from all sources once?k
+        if (fetchResult?.error) {
+          yield {
+            status: "error",
+            error: {
+              code: "FETCH_FAILED",
+              message: fetchResult.error.message,
+            },
+          };
+        } else {
+          fetchResult?.feed.forEach((item) => {
+            fetchedArray.push({
+              ...item,
+              categories: null,
+            });
+          });
+        }
       }
 
-      // TODO: Unflatten cached into article 1 <-> M categories
+      const cached = await db
+        .selectFrom("cached_articles")
+        .where("published_at", ">=", input)
+        .leftJoin(
+          "article_categories",
+          "article_categories.article_id",
+          "cached_articles.id",
+        )
+        .select([
+          "id",
+          "fetch_id",
+          "title",
+          "link",
+          "author",
+          "published_at",
+          "used",
+          "category",
+        ])
+        .execute();
 
-      return {
+      const agent = await db
+        .selectFrom("agents")
+        .where("user_id", "=", ctx.session.user.id)
+        .where("enabled", "=", 1)
+        .select(["url", "model", "api_key"])
+        .executeTakeFirstOrThrow();
+      let classified = [] as ArticleWithCategories[];
+
+      const endpoint = agent.url.match(
+        /(?<=https?:\/\/.*\/).*/,
+      )![0] as AgentEndpoint;
+
+      const agentAdapter = AgentAdapterFactory(endpoint);
+
+      if (cached.length === 0) {
+        if (fetchedArray.length === 0) return;
+
+        await db.insertInto("cached_articles").values(fetchedArray!).execute();
+
+        classified = await ClassifyArticles(agent, agentAdapter, fetchedArray);
+      } else {
+        const cachedUnflattened = UnflattedCached(cached);
+
+        const [cacheMisses, cachedCatz, cachedUncatz] = ExtractUnusedArticles(
+          fetchedArray,
+          cachedUnflattened,
+        ); // FIX: Why tuple's arrays are possibly undefined
+
+        // Then use agentic classification for cacheMisses & cachedUncatz (union)
+        // then unify newly_cassificied & cashedCatz for digest gen
+
+        await db.insertInto("cached_articles").values(cacheMisses!).execute();
+
+        const uncatz = cacheMisses!;
+
+        cachedUncatz!.forEach((article) => uncatz.push(article));
+
+        // generate from Cache misses + unused caches
+
+        // INFO: Rate limiting
+
+        classified = await ClassifyArticles(uncatz);
+
+        cachedCatz?.forEach((article) => classified.push(article));
+      }
+
+      // TODO: batching logic + yield return
+
+      GenerateDigests(classified);
+
+      // TODO: Digests Generation
+
+      // TODO: Update "used" field for articles
+
+      yield {
         status: "success",
       };
     }),
--- src/server/lib/util.ts
+++ src/server/lib/util.ts
@@ -1,26 +1,35 @@
+import { AGENT, type AgentEndpoint } from "~/config/business";
+import { OAIAdapter, type AgentAdapter } from "~/lib/adapters/agent";
+
 export async function TestOAIAPI(url: string, apiKey: string) {
-    const res = await fetch(`${url}/openai/v1/models`, {
-        headers: { Authorization: `Bearer ${apiKey}` }
-    })
+  const res = await fetch(`${url}/openai/v1/models`, {
+    headers: { Authorization: `Bearer ${apiKey}` },
+  });
 
-    let errorMessage = null
+  let errorMessage = null;
 
-    if (!res.ok) {
-        const error = await res.json()
-        errorMessage = error.error?.message ?? `HTTP ${res.status}`
-    }
+  if (!res.ok) {
+    const error = await res.json();
+    errorMessage = error.error?.message ?? `HTTP ${res.status}`;
+  }
 
-    return {
-        status: res.status,
-        error: errorMessage
-    }
+  return {
+    status: res.status,
+    error: errorMessage,
+  };
 }
 
 export function IsDuplicateEntry(err: unknown, key?: string): boolean {
-    return (
-        err instanceof Error &&
-        "code" in err &&
-        err.code === "ER_DUP_ENTRY" &&
-        (key ? err.message.includes(key) : true)
-    )
+  return (
+    err instanceof Error &&
+    "code" in err &&
+    err.code === "ER_DUP_ENTRY" &&
+    (key ? err.message.includes(key) : true)
+  );
+}
+
+export function isOAIAdapter(
+  adapter: AgentAdapter,
+): adapter is typeof OAIAdapter {
+  return adapter.endpoint === AGENT.SUPPORTED_ENDPOINTS.OpenAI;
 }
