084209c8d0f3f01f8dc09432a1dd2ae4c85e600f
Ksenox <akseli.muhonen@proton.me>
2026-04-04 13:38:50 +0300

    style(category): formatting

--- src/server/api/routers/category.ts
+++ src/server/api/routers/category.ts
@@ -1,123 +1,131 @@
 import { z } from "zod";
-import { createTRPCRouter, adminProcedure, protectedProcedure, protectedTranslatedProcedure } from "~/server/api/trpc";
+import {
+  createTRPCRouter,
+  adminProcedure,
+  protectedProcedure,
+  protectedTranslatedProcedure,
+} from "~/server/api/trpc";
 import { db } from "~/server/db/db";
 import { CategorySchemaFactory } from "~/lib/validators/category";
 import { IsDuplicateEntry } from "~/server/lib/util";
 
 export const categoryRouter = createTRPCRouter({
-    getCategories: protectedProcedure
-        .query(async ({ ctx }) => {
-            const [allCategories, userCategories] = await Promise.all([
-                db.selectFrom("categories").selectAll().execute(),
-                db
-                    .selectFrom("user_categories")
-                    .select("category")
-                    .where("user_id", "=", ctx.session.user.id)
-                    .execute(),
-            ]);
+  getCategories: protectedProcedure.query(async ({ ctx }) => {
+    const [allCategories, userCategories] = await Promise.all([
+      db.selectFrom("categories").selectAll().execute(),
+      db
+        .selectFrom("user_categories")
+        .select("category")
+        .where("user_id", "=", ctx.session.user.id)
+        .execute(),
+    ]);
 
-            const userCategorySet = new Set(userCategories.map((uc) => uc.category));
+    const userCategorySet = new Set(userCategories.map((uc) => uc.category));
 
-            const categoriesHash: Record<string, boolean> = {};
+    const categoriesHash: Record<string, boolean> = {};
 
-            for (const category of allCategories) {
-                categoriesHash[category.slug] = userCategorySet.has(category.slug);
-            }
+    for (const category of allCategories) {
+      categoriesHash[category.slug] = userCategorySet.has(category.slug);
+    }
 
-            return {
-                status: "success",
-                categories: categoriesHash,
-            };
-        }),
-    selectCategory: protectedTranslatedProcedure
-        .input(z.unknown())
-        .mutation(async ({ ctx, input }) => {
-            const schema = CategorySchemaFactory(ctx.t)
-            const validated = schema.parse(input)
+    return {
+      status: "success",
+      categories: categoriesHash,
+    };
+  }),
+  selectCategory: protectedTranslatedProcedure
+    .input(z.unknown())
+    .mutation(async ({ ctx, input }) => {
+      const schema = CategorySchemaFactory(ctx.t);
+      const validated = schema.parse(input);
 
-            await db
-                .insertInto("user_categories")
-                .values({
-                    user_id: ctx.session.user.id,
-                    category: validated,
-                })
-                .onConflict((oc) => oc.doNothing())
-                .execute();
+      await db
+        .insertInto("user_categories")
+        .values({
+          user_id: ctx.session.user.id,
+          category: validated,
+        })
+        .onConflict((oc) => oc.doNothing())
+        .execute();
 
-            return { status: "success" };
-        }),
-    deselectCategory: protectedTranslatedProcedure
-        .input(z.unknown())
-        .mutation(async ({ ctx, input }) => {
-            const schema = CategorySchemaFactory(ctx.t)
-            const validated = schema.parse(input)
+      return { status: "success" };
+    }),
+  deselectCategory: protectedTranslatedProcedure
+    .input(z.unknown())
+    .mutation(async ({ ctx, input }) => {
+      const schema = CategorySchemaFactory(ctx.t);
+      const validated = schema.parse(input);
 
-            const result = await db
-                .deleteFrom("user_categories")
-                .where("user_id", "=", ctx.session.user.id)
-                .where("category", "=", validated)
-                .execute();
+      const result = await db
+        .deleteFrom("user_categories")
+        .where("user_id", "=", ctx.session.user.id)
+        .where("category", "=", validated)
+        .execute();
 
-            if (result[0]!.numDeletedRows === 0n) {
-                return {
-                    status: "failure",
-                    errorCode: "NOT_FOUND",
-                    error: "Category not found"
-                };
-            }
+      // TODO: translate error message
 
-            return { status: "success" };
-        }),
-    addCategory: adminProcedure
-        .input(z.unknown())
-        .mutation(async ({ ctx, input }) => {
-            const schema = CategorySchemaFactory(ctx.t)
-            const validated = schema.parse(input)
+      if (result[0]!.numDeletedRows === 0n) {
+        return {
+          status: "failure",
+          errorCode: "NOT_FOUND",
+          error: "Category not found",
+        };
+      }
 
-            try {
-                await db
-                    .insertInto("categories")
-                    .values({
-                        slug: validated,
-                    })
-                    .execute();
+      return { status: "success" };
+    }),
+  addCategory: adminProcedure
+    .input(z.unknown())
+    .mutation(async ({ ctx, input }) => {
+      const schema = CategorySchemaFactory(ctx.t);
+      const validated = schema.parse(input);
 
-                return { status: "success" };
-            } catch (err: any) {
-                if (IsDuplicateEntry(err, "slug")) {
-                    return {
-                        status: "failure",
-                        errorCode: "CONFLICT",
-                        error: ctx.t('errors.conflicts.categoryExists'),
-                    };
-                }
+      try {
+        await db
+          .insertInto("categories")
+          .values({
+            slug: validated,
+          })
+          .execute();
 
-                return {
-                    status: "failure",
-                    errorCode: err.code,
-                    error: err.message,
-                };
-            }
-        }),
-    removeCategory: adminProcedure
-        .input(z.unknown())
-        .mutation(async ({ ctx, input }) => {
-            const schema = CategorySchemaFactory(ctx.t)
-            const validated = schema.parse(input)
+        return { status: "success" };
+      } catch (err: any) {
+        if (IsDuplicateEntry(err, "slug")) {
+          return {
+            status: "failure",
+            errorCode: "CONFLICT",
+            error: ctx.t("errors.conflicts.categoryExists"),
+          };
+        }
 
-            const result = await db
-                .deleteFrom("categories")
-                .where("slug", "=", validated)
-                .execute();
+        return {
+          status: "failure",
+          errorCode: err.code,
+          error: err.message,
+        };
+      }
+    }),
+  removeCategory: adminProcedure
+    .input(z.unknown())
+    .mutation(async ({ ctx, input }) => {
+      const schema = CategorySchemaFactory(ctx.t);
+      const validated = schema.parse(input);
 
-            if (result[0]!.numDeletedRows === 0n) {
-                return {
-                    status: "failure",
-                    errorCode: "NOT_FOUND",
-                    error: "Category not found"
-                };
-            }
+      const result = await db
+        .deleteFrom("categories")
+        .where("slug", "=", validated)
+        .execute();
 
-            return { status: "success" };
-        }),
+      // TODO: translate error message
+
+      if (result[0]!.numDeletedRows === 0n) {
+        return {
+          status: "failure",
+          errorCode: "NOT_FOUND",
+          error: "Category not found",
+        };
+      }
+
+      return { status: "success" };
+    }),
 });
